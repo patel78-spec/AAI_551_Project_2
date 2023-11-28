93d0448fb6ce07204b7e9e9f20032aef7502ddc8
Dhruv Patel <dhruvpatel.r78@gmail.com>
2023-11-27 19:19:50 -0500

    Added todos

--- GameEngine.py
+++ GameEngine.py
@@ -1,3 +1,75 @@
-# Name:
+# todo 
+# Authors: {Your Name}, Dhruv Patel
 # Date:
 # Description:
+
+import random
+import pickle
+from Veggie import Veggie
+from Captain import Captain
+from Rabbit import Rabbit
+
+class GameEngine:
+  NUMBEROFVEGGIES = 30
+  NUMBEROFRABBITS = 5
+  HIGHSCOREFILE = 'highscore.data'
+  
+  # todo constructor by cora
+  def __init__(self):
+    pass
+  
+  # todo by cora
+  def initVeggies():
+    pass
+  
+  # todo by cora
+  def initCaptain():
+    pass
+  
+  # todo by cora
+  def initRabbit():
+    pass
+  
+  # todo by cora
+  def initializeGame():
+    pass
+  
+  # todo by cora
+  def remainingVeggies():
+    pass
+  
+  # todo by cora
+  def printField():
+    pass
+  
+  # todo by dhruv
+  def printField():
+    pass
+  
+  # todo by dhruv
+  def getScore():
+    pass
+  
+  # todo by dhruv
+  def moveRabbits():
+    pass
+  
+  # todo by dhruv
+  def moveCptVertical():
+    pass
+  
+  # todo by dhruv
+  def moveCptHorizontal():
+    pass
+  
+  # todo by dhruv
+  def moveCaptain():
+    pass
+  
+  # todo by dhruv
+  def gameOver():
+    pass
+  
+  # todo by dhruv
+  def highScore():
+    pass
